e7b6a2f0602066bb36ea076e6bbd66c36d697e2c
fuyanwei <fu1665588373@live.com>
2019-11-26 21:53:20 +0800

    x

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -3,8 +3,72 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
+CKEDITOR.addCss( 'figure[class*=easyimage-gradient]::before { content: ""; position: absolute; top: 0; bottom: 0; left: 0; right: 0; }' +
+'figure[class*=easyimage-gradient] figcaption { position: relative; z-index: 2; }' +
+'.easyimage-gradient-1::before { background-image: linear-gradient( 135deg, rgba( 115, 110, 254, 0 ) 0%, rgba( 66, 174, 234, .72 ) 100% ); }' +
+'.easyimage-gradient-2::before { background-image: linear-gradient( 135deg, rgba( 115, 110, 254, 0 ) 0%, rgba( 228, 66, 234, .72 ) 100% ); }' );
+
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+
+	// The toolbar groups arrangement, optimized for a single toolbar row.
+	config.toolbarGroups = [
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'forms' },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'tools' },
+		{ name: 'others' },
+		{ name: 'about' },
+	];
+
+	// The default plugins included in the basic setup define some buttons that
+	// are not needed in a basic editor. They are removed here.
+	// config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+	config.extraPlugins = 'easyimage,blockquote';
+	config.easyimage_styles = {
+        gradient1: {
+          group: 'easyimage-gradients',
+          attributes: {
+            'class': 'easyimage-gradient-1'
+          },
+          label: 'Blue Gradient',
+          icon: 'https://ckeditor.com/docs/ckeditor4/4.13.0/examples/assets/easyimage/icons/gradient1.png',
+          iconHiDpi: 'https://ckeditor.com/docs/ckeditor4/4.13.0/examples/assets/easyimage/icons/hidpi/gradient1.png'
+        },
+        gradient2: {
+          group: 'easyimage-gradients',
+          attributes: {
+            'class': 'easyimage-gradient-2'
+          },
+          label: 'Pink Gradient',
+          icon: 'https://ckeditor.com/docs/ckeditor4/4.13.0/examples/assets/easyimage/icons/gradient2.png',
+          iconHiDpi: 'https://ckeditor.com/docs/ckeditor4/4.13.0/examples/assets/easyimage/icons/hidpi/gradient2.png'
+        },
+        noGradient: {
+          group: 'easyimage-gradients',
+          attributes: {
+            'class': 'easyimage-no-gradient'
+          },
+          label: 'No Gradient',
+          icon: 'https://ckeditor.com/docs/ckeditor4/4.13.0/examples/assets/easyimage/icons/nogradient.png',
+          iconHiDpi: 'https://ckeditor.com/docs/ckeditor4/4.13.0/examples/assets/easyimage/icons/hidpi/nogradient.png'
+        }
+      }
+	config.easyimage_toolbar = [ 
+		'EasyImageAlignLeft', 'EasyImageAlignCenter', 'EasyImageAlignRight', 'EasyImageAlt',
+        'EasyImageGradient1',
+        'EasyImageGradient2',
+        'EasyImageNoGradient', ];
+	// Dialog windows are also simplified.
+  config.removeDialogTabs = 'link:advanced';
+  config.removePlugins = 'cloudservices';
 };
