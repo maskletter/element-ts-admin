1fe2035ae1a924731a1f43d1e309596a799ba9a5
fuyanwei <fu1665588373@live.com>
2019-12-08 16:02:01 +0800

    x

--- src/views/ts/main.ts
+++ src/views/ts/main.ts
@@ -5,24 +5,83 @@ import { State } from 'vuex-class'
 import { RouteConfig } from "vue-router";
 // import request from 'tool/request'
 // import permission from '../../permission'
-
+const dialogVue = Vue.extend({
+    template: `
+        <el-dialog :title="title" :visible.sync="cropperShow">
+            <div ref='div'></div>
+        </dialog>
+    `,
+    data(){
+        return {
+            cropperShow: false,
+            title: ''
+        }
+    },
+    methods: {
+        open(a: any, title: string){
+            setTimeout(() => {
+                this.title = title
+                a.$mount(this.$refs.div)
+            })
+        }
+    }
+})
+const MlDisableMenuRouter = {
+    render: Vue2.compile(`
+        <div>
+            <slot></slot>
+        </div>
+    `).render,
+    props: ['item'],
+    mounted(){
+        const $this = this as any
+        const $div = document.createElement('div')
+        $div.setAttribute('style', 'position:absolute;left:0px;top:0px;right:0px;bottom:0px;');
+        $this.$parent.$el.appendChild($div)
+        $this.item.meta.dialog && $div.addEventListener('click', (e) => {
+            e.stopPropagation();
+            $this.showDialog($this.item)
+        })
+    },
+    methods: {
+        async showDialog(item: any){
+            const $div = document.createElement('div');
+            document.body.appendChild($div)
+            const result = await item.component();
+            const $component = new dialogVue()
+            $component.$mount($div);
+            $component.cropperShow = true;
+            $component.open(new result.default(), item.meta.title)
+            
+        }
+    }
+}
 const MlMenu = {
     name: 'router-menu',
+    components:{ MlDisableMenuRouter },
     render: Vue2.compile(`
         <div>
             <template v-for='item in routers' >
-                <el-submenu v-if='item.children && item.children.length' :index="'/'+item.path" @click.native='click([item.path])'>
+                <el-submenu v-if='item.children && item.children.length' :index="(parentPath?'/'+parentPath:parentPath)+'/'+item.path" @click.native='click([item.path])'>
                     <template slot="title"><i :class="item.meta.icon||'el-icon-platform-eleme'"></i>{{item.meta.title}}</template>
-                    <el-menu-item @click.native='click([item.path,item2.path])' v-for='item2 in item.children' :index="'/'+item.path+'/'+item2.path">{{item2.meta.title}}</el-menu-item>
+                    <router-menu :routers='item.children' :parentPath='parentPath?parentPath+"/"+item.path:item.path' />
                 </el-submenu>
-                <el-menu-item v-else :index="'/'+item.path" @click.native='click([item.path])'>
-                    <i :class="item.meta.icon||'el-icon-platform-eleme'"></i>
-                    <span slot="title">{{item.meta.title}}</span>
+                <el-menu-item v-else :index="(parentPath?'/'+parentPath:parentPath)+'/'+item.path" @click.native='click([item.path])'>
+                    <ml-disable-menu-router :item='item'>
+                        <i :class="item.meta.icon||'el-icon-platform-eleme'"></i>
+                        <span >{{item.meta.title}}</span>
+                    </ml-disable-menu-router>
                 </el-menu-item>
             </template>
         </div>
     `).render,
-    props: ['routers'],
+    // props: ['routers','parentPath'],
+    props: {
+        routers: {},
+        parentPath: {
+            default: ""
+        }
+    },
     methods: {
         click(e: string[]){
             (this as any).$emit('change', e.map(v => '/'+v));
@@ -35,10 +94,10 @@ const MlMenu = {
 export default class MainComponent extends Vue{
 
     @State('routers') private routers!: RouteConfig[]
-    private menuOpeneds: string[] = [];
-    private isCollapse: boolean = false
+    @State('mode') private mode!: string
 
     private async logout(): Promise<void> {
+        
         await this.$confirm('确定退出吗').toPromise()
         sessionStorage.removeItem('login');
         this.$router.replace('/login');
